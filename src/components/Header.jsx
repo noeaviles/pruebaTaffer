0ad305736197b230de42a60a7cc928b20bc2f904
noeaviles <noe.alexander.aviles@gmail.com>
2023-05-13 12:09:20 -0600

    Se corrigen header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
   }, [])
   
   return (
-    <header className="Header" style={{ backgroundImage: `url(${portada})`}}>
+    <header className="Header" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),url(${portada})`}}>
       <Nav/>
-      <div className="Header-fondo " >
+      <div className="Header-fondo ">
         <h1 className="Header-h1 animate" data-rellax-zindex="2">Garza Blanca Resorts</h1>
         <h2 className="Header-h2  animate" data-rellax-zindex="2">BY TAFER HOTELS & RESORTS</h2> 
         <div className="Header-logos animate" data-rellax-zindex="2">
